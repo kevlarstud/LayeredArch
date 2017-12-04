1adf9e80f126a636330730ed54ff13f2420ee2cd
kevlarstud <raghavagrawalr95@gmail.com>
2017-12-04 09:59:30 +0530

    Add files via upload

---
+++ GuestPhoneBook/GuestPhoneBook.DataAccessLayer/PhoneBookConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace GuestPhoneBook.DataAccessLayer
+{
+    public class PhoneBookConfiguration
+    {
+        private static string providerName;
+
+        public static string ProviderName
+        {
+            get { return PhoneBookConfiguration.providerName; }
+            set { PhoneBookConfiguration.providerName = value; }
+        }
+        private static string connectionString;
+
+        public static string ConnectionString
+        {
+            get { return PhoneBookConfiguration.connectionString; }
+            set { PhoneBookConfiguration.connectionString = value; }
+        }
+
+        static PhoneBookConfiguration()
+        {
+            providerName = ConfigurationManager.ConnectionStrings["phoneBookConnection"].ProviderName;
+            connectionString = ConfigurationManager.ConnectionStrings["phoneBookConnection"].ConnectionString;
+
+        }
+    }
+}
