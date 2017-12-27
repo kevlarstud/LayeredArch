8e663dfb94aeb8f31b7a3d945b80e5af232db46b
kevlarstud <raghavagrawalr95@gmail.com>
2017-12-27 09:09:42 +0530

    Add files via upload

---
+++ Student Admissio/Exceptions/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    public class AdmissionException : ApplicationException
+    {
+        public AdmissionException()
+        {
+
+        }
+        public AdmissionException(string Exception) : base(Exception)
+        {
+
+        }
+        public AdmissionException(string message, Exception innerException) : base(message)
+        {
+
+        }
+    }
+    
+}
